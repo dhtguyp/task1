01d1b87a4f7c06c475f90ce762d41eb68f9d7c06
yotamdinar <dinaryo@post.bgu.ac.il>
2023-04-12 20:10:11 +0300

    task3

---
+++ user/goodbye.c
@@ -0,0 +1,11 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+int
+main(int argc, char *argv[])
+{
+  char * msg = "Goodbye World xv6";
+  printf("hi");
+  exit(0, msg);
+}
+
